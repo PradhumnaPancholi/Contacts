ebb9288b1e465c2ae6e609922024a231256998bd
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-09-24 15:40:04 -0400

    created basic instance variables, constructor, getters and setters for model Contacts

--- src/Models/Contacts.java
+++ src/Models/Contacts.java
@@ -1,4 +1,69 @@
 package Models;
 
-public class Contacts {
+import javafx.scene.image.Image;
+
+public class Contacts<date> {
+    private String firstName, lastName, Address;
+    private date dateOfBirth;
+    private double phone;
+    private Image image;
+
+    public Contacts(String firstName, String lastName, String address, date dateOfBirth, double phone, Image image) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAddress(address);
+        setDateOfBirth(dateOfBirth);
+        setPhone(phone);
+        setImage(image);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public double getPhone() {
+        return phone;
+    }
+
+    public void setPhone(double phone) {
+        this.phone = phone;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
 }
+
+

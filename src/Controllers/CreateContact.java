dd896051d17819138c5dbccc1d17fc33e4c4a4f0
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-10-06 15:46:09 -0400

    added default imageView

--- src/Controllers/CreateContact.java
+++ src/Controllers/CreateContact.java
@@ -1,14 +1,24 @@
 package Controllers;
 
 import Models.Contact;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+
+import javax.imageio.ImageIO;
+import javafx.scene.image.ImageView;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class CreateContact {
+public class CreateContact implements Initializable {
 
     @FXML
     private TextField firstNameTextField;
@@ -26,6 +36,9 @@ public class CreateContact {
     private TextField phoneTextField;
 
     @FXML
+    private ImageView imageView;
+
+    @FXML
     private Button chooseImageButton;
 
     @FXML
@@ -36,6 +49,8 @@ public class CreateContact {
 
     @FXML
     private Button cancelButton;
+
+    private File image;
 
     //this method will try to fetch data from GUI, run validation and save it into database//
     public void saveButtonPushed(javafx.event.ActionEvent actionEvent)
@@ -48,6 +63,20 @@ public class CreateContact {
         } catch (SQLException e) {
             output.setText(e.getMessage());
 
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //to load default image//
+        try{
+            image = new File("./src/img/default.png");
+            BufferedImage bufferedImage = ImageIO.read(image);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            imageView.setImage(image);
+        }
+        catch (IOException e){
+            System.err.println(e.getMessage());
         }
     }
 }
